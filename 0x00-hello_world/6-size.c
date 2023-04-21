b47827f35f8d7295c03c7aad3b4340784eb981cc
Rukkieee <ruqayyahibrahim983@gmail.com>
2023-04-20 18:34:00 -0700

    Task 6c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("size of a float: %lu byte(S)\n", sizeof(float));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 
